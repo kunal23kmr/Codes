45500493a46bb43ac67c48161ef3d5a41a441576
kunal <kunal23kmr@gmail.com>
2023-10-31 12:32:05 +0530

    server-stuf too

--- ReactJs/FoodieStudio/backend/server.js
+++ ReactJs/FoodieStudio/backend/server.js
@@ -1,45 +1,3 @@
-// const express = require("express");
-// const mysql = require('mysql');
-
-// const cors = require('cors');
-
-// const app = express();
-// app.use(cors);
-// app.use(express.json());
-
-
-// const db = mysql.createConnection({
-//     host: "localhost",
-//     user: "root",
-//     password: "12345",
-//     database: "foodiestudio"
-// })
-
-
-// app.post('/signup', (req, res) => {
-//     const sql = "INSERT INTO users (`first_name`, `last_name`, `mobile_number`, `city`, `state`, `country`, `pincode`, `password`) VALUES(?) ";
-//     const values = [
-//         req.body.first_name,
-//         req.body.last_name,
-//         req.body.mobile_number,
-//         req.body.city,
-//         req.body.state,
-//         req.body.country,
-//         req.body.pincode,
-//         req.body.password
-//     ];
-//     db.query(sql, [values], (err, data) => {
-//         if (err) {
-//             return res.json("Error");
-//         }
-//         return res.json(data);
-//     })
-// })
-
-// app.listen(3001, () => {
-//     console.log("listening");
-// })
-
 const express = require("express");
 const mysql = require('mysql2');
 
@@ -112,6 +70,7 @@ app.post('/signup', (req, res) => {
 });
 
 
+
 app.post('/login', (req, res) => {
     const mobileNumber = req.body.mobile_number;
     const password = req.body.password;
@@ -119,6 +78,31 @@ app.post('/login', (req, res) => {
     console.log(mobileNumber);
     console.log(password);
     db.query('SELECT * FROM users WHERE mobile_number = ? and password = ?', [mobileNumber, password], (err, results) => {
+        // Query execution and callback
+        if (err) {
+            console.error('Error:', err);
+            return res.status(500).json({ message: 'Error' });
+        }
+        if (results.length > 0) {
+            console.log('mil gya');
+            return res.status(200).json(results);
+        }
+        console.log("baigan");
+        return res.status(400).json({ message: 'Wrong phone number or, password.' });
+
+    });
+});
+
+
+app.post('/viewProfile', (req, res) => {
+    // const mobileNumber = req.body.mobile_number;
+    // const password = req.body.password;
+
+    const id = req.body.user_id;
+
+    console.log(id);
+    // console.log(password);
+    db.query('SELECT user_id,first_name,last_name,mobile_number,city,state,country,pincode FROM users WHERE user_id= ?', [user_id], (err, results) => {
         // Query execution and callback
         if (err) {
             console.error('Error:', err);
