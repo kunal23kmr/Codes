d305e7029ac2fd103219e6e084aa2bfd0b0b1473
Kunal Kumar <kunal23kmr@gmail.com>
2024-01-03 19:39:13 +0530

    empty

---
+++ test/test.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define Mod 1000000007
+#define Mod1 998244353
+#define inf 1e18
+#define pb push_back
+#define ppb pop_back
+typedef long long ll;
+
+void solve()
+{
+   int t;
+   cin >> t;
+   while (t--)
+   {
+      // Your code here
+   }
+}
+
+int main()
+{
+   solve();
+   return 0;
+}
